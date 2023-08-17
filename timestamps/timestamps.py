c93a2b0d02f1ed25a67baf0faecedd2378e999ff
Noah Meneghini <noah.meneghini@gmail.com>
2023-08-17 11:57:35 -0400

    Try again

--- timestamps/timestamps.py
+++ timestamps/timestamps.py
@@ -1,40 +1,30 @@
-import datetime
-
+from datetime import datetime
 import dateparser
 import discord
 from redbot.core import commands
 from redbot.core.bot import Red, app_commands
-from redbot.core.commands import BadArgument, Cog, Context, Converter
-
-
-class DateConverter(Converter):
-    """Date converter which uses dateparser.parse()."""
-
-    async def convert(self, ctx: Context, arg: str) -> datetime.datetime:
-        parsed = dateparser.parse(arg)
-        if parsed is None:
-            raise BadArgument("Unrecognized date/time.")
-        return parsed
+from redbot.core.commands import BadArgument, Cog, Converter
 
 
 class TimeStamps(Cog):
     """Retrieve timestamps for certain dates."""
 
-    __author__ = "Kreusada"
-    __version__ = "1.1.0"
+    __author__ = "noahdm4321"
+    __version__ = "1.0"
 
     def __init__(self, bot: Red):
         self.bot = bot
 
-    def format_help_for_context(self, ctx: commands.Context) -> str:
-        context = super().format_help_for_context(ctx)
-        return f"{context}\n\nAuthor: {self.__author__}\nVersion: {self.__version__}"
-
-    async def red_delete_data_for_user(self, **kwargs):
-        return
+    async def convert(self, arg) -> datetime:
+        """Date converter which uses dateparser.parse()."""
+        parsed = dateparser.parse(arg)
+        if parsed is None:
+            raise BadArgument("Unrecognized date/time.")
+        return parsed
 
     @app_commands.command()
-    async def timestamp(self, interaction: discord.Interaction, author: Context.author, member: discord.Member, embed: Context.embed_requested, colour: Context.embed_colour, dti: DateConverter):
+    @app_commands.describe(time="The time you want")
+    async def timestamp(self, interaction: discord.Interaction, time: str):
         """Produce a Discord timestamp.
 
         Timestamps are a feature added to Discord in the summer of 2021,
@@ -43,14 +33,14 @@ class TimeStamps(Cog):
 
         **Example Usage**
 
-        - `[p]timestamp 1st of october, 2021`
-        - `[p]timestamp 20 hours ago`
-        - `[p]timestamp in 50 minutes`
-        - `[p]timestamp 01/10/2021`
-        - `[p]timestamp now`
+        - `/timestamp 1st of october, 2021`
+        - `/timestamp 20 hours ago`
+        - `/timestamp in 50 minutes`
+        - `/timestamp 01/10/2021`
+        - `/timestamp now`
         """
         try:
-            ts = int(dti.timestamp())
+            ts = int(self.convert(time))
         except OSError:
             await interaction.response.send_message(
                 "An operating system error occured whilst attempting to get "
@@ -64,13 +54,4 @@ class TimeStamps(Cog):
             message += f"`<t:{ts}:{i.upper()}>`: <t:{ts}:{i.upper()}>\n"
             message += f"`<t:{ts}:{i.lower()}>`: <t:{ts}:{i.lower()}>\n"
         message += f"`<t:{ts}:R>`: <t:{ts}:R>\n"
-        if await embed:
-            await interaction.response.send_message(
-                content=ts
-                if isinstance(author, member) and author.is_on_mobile()
-                else None,
-                embed=discord.Embed(description=message, color=(await colour)),
-                ephemeral=True,
-            )
-        else:
-            await interaction.response.send_message(message,ephemeral=True)
+        await interaction.response.send_message(message,ephemeral=True)
